fed8bf93ffe59359ff5ba3e68489f6ed4c6d7dd0
Richard Soriano <59493601+richardsoriano@users.noreply.github.com>
2022-12-11 19:44:14 -0800

    bug-bag-module2

--- src/features/footer/index.jsx
+++ src/features/footer/index.jsx
@@ -1,6 +1,6 @@
 export default function Footer({}) {
   return (
-    <div className='text-xs py-6 text-center'>
+    <div className="py-6 text-xs text-center">
       Copyright © 2021 The High Plains Book Awards
     </div>
   )
